eaab997750420f2fc11c748c9009762a2b08fd2a
anandcheerla <31164268+anandcheerla@users.noreply.github.com>
2018-08-05 21:42:54 +0530

    Added set data structure to remove duplicates

--- vocabulary.py
+++ vocabulary.py
@@ -14,28 +14,35 @@ count=1
 if(input()=='yes'):
 	print("This may take sometime as we need to get every vocabulary meaning",end="\n")
 	print("Working.....")
-	with open(input_file,'r',encoding='utf-8') as f:
+	words=set()
+	with open(input_file,'r') as f:
 		for line in f:
-			#print(line)
 			for word in line.split():
 				word=re.sub('[^a-zA-Z]', '', word)
-				if word not in li and len(word)>=int(inp):
-					html=BeautifulSoup(requests.get("https://www.google.co.in/search?q=meaning:{0}".format(word)).text,'html.parser')
-					table=html.find("table", {"style": "font-size:14px;width:100%"})
-					#print(word)
-					if table != None:
-						#print(word)
-						out.write(word+"\n")
-						meaning = table.find_all("li")
-						out_meanings.write(word+" : "+"\n")
-						for i in meaning:
-							out_meanings.write(i.getText()+"\n")
-						out_meanings.write("\n")
-						print("finished "+str(count),end="\r")
-						count+=1
-						
+				if word != "" and word not in li and len(word)>=int(inp):
+					words.add(word)
+	
+	words=list(words)
+	for i in words:
+		out.write(i+"\n")
+					
 	print("The vocabulary words are generated and is stored in output file")
-	print("The vocabulary meanings are generated and stored in output_meanings file")
+	if(input("If you want meanings of the vocabulary, input yes")):
+		for word in words:
+			html=BeautifulSoup(requests.get("https://www.google.co.in/search?q=meaning:{0}".format(word)).text,'html.parser')
+			table=html.find("table", {"style": "font-size:14px;width:100%"})
+			if table != None:
+				meaning = table.find_all("li")
+				out_meanings.write(word+" : "+"\n")
+				for i in meaning:
+					out_meanings.write(i.getText()+"\n")
+				out_meanings.write("\n")
+				print("finished "+str(count),end="\r")
+				count+=1
+					
+		print("The vocabulary meanings are generated and stored in output_meanings file")
+	else:
+		print("Thank You")
 else:
 	print("Thank You")
 	
